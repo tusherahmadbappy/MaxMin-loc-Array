f09da817aff7959159eac9f825220d4c3c7039d3
Md. Tusher Ahmad Bappy <106505665+tusherahmadbappy@users.noreply.github.com>
2022-06-02 21:05:20 +0600

    Find out the maximum & Minimum value & location
    Find out the maximum & Minimum value  with their location of an array.

---
+++ maxMinLocArray.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+void big(int a[],int max,int maxloc,int num);
+void smallest(int a[], int min, int minloc,int num);
+
+void big(int a[],int max,int maxloc,int num)
+{
+
+    for(int i=2;i<=num;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+            maxloc=i;
+        }
+    }
+    cout<<"\nMaximum is :"<<max<<endl;
+    cout<<"Maximum location is :"<<maxloc<<endl;
+}
+
+void smallest(int a[], int min, int minloc,int num)
+{
+    int i;
+    for(int i=2;i<=num;i++)
+    {
+        if(a[i]<min)
+        {
+            min=a[i];
+            minloc = i;
+        }
+    }
+
+    cout<<"\nMinimum is: "<<min<<endl;
+    cout<<"Minimum location is: "<<minloc<<endl;
+
+}
+
+int main()
+{
+    int num;
+    cout<<"Enter the size of array: ";
+    cin>>num;
+    int arr[num];
+    for(int i=1;i<=num;i++)
+    {
+        cout<<"Enter element "<<i<<" index element: ";
+        cin>>arr[i];
+    }
+
+    int max=arr[1],maxloc=1;
+    int min=arr[1],minloc=1;
+
+    big(arr,max,maxloc,num);
+    smallest(arr,min,minloc,num);
+
+
+    return 0;
+
+}
